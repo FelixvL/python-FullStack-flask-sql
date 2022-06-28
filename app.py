779a0df173aa0fe59f2d98c351ac73f86d5ed0d6
FelixvL <digitaleservice@gmail.com>
2022-06-28 18:58:08 +0200

    frontend werkend

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask
 import mysql.connector
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
 
 def optellen():
     return 6 + 14
 
 @app.route("/")
+@cross_origin()
 def hello_world():
     return "<p>Hello, World!</p>" + str(optellen())
 
 
 @app.route("/andereingang")
+@cross_origin()
 def tweedefunctie():
     mydb = mysql.connector.connect(
         host="localhost",  #port erbij indien mac
